f717f9add0e9378c0d23da5b0fb7dfddee1e410b
tran hoang dai <tranhoangdainguyen@gmail.com>
2018-07-17 09:22:05 +1000

    wip

--- surprise_deep/algorithms/autoencoder.py
+++ surprise_deep/algorithms/autoencoder.py
@@ -140,7 +140,7 @@ class Autoencoder(AlgoBaseDeep):
                 inputs = mini_batch.to_dense().to(device=self.device)
                 outputs = self.forward(inputs)
                 loss = self.MMSEloss(outputs, inputs)
-                if np.isnan(loss):
+                if math.isnan(loss):
                     continue #todo:check nan loss
                 self.logger.debug_(f'epoch {epoch} - loss:{loss}')
                 loss.backward()
