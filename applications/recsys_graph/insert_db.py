9ca37521b2681cdc87c430976f24fe8051cd5875
daitran <daitran@narantech.com>
2018-08-10 12:19:14 +1000

    wip for neo4j

--- applications/recsys_graph/insert_db.py
+++ applications/recsys_graph/insert_db.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('../..')
 sys.path.append('..')
 print(sys.path)
@@ -8,27 +9,65 @@ import json
 import pandas as pd
 from surprise_deep import *
 
-from db import neo4jdb
+from py2neo import Graph
+from py2neo import Node
 
-ds_ml100k_opt = DatasetOption()
+graph = Graph("bolt://128.199.71.16:7687", auth=("neo4j", "DaiNeo4j123!@#"))
+ds_ml100k_opt = None
+
 
 def download_ml100k():
     global ds_ml100k_opt
-    ds_ml100k_opt = DatasetOption(g_root_dir='ds',g_save_dir='100k')
+    ds_ml100k_opt = DatasetOption(g_root_dir='ds', g_save_dir='100k')
     ds_ml100k = Movielens(ds_ml100k_opt)
     ds_ml100k.download_and_process_data()
 
-def insert_ml100k():
-    raw_movies_db_path = os.path.join(ds_ml100k_opt.get_working_dir(),'raw','ml-latest-small','movies.csv')
+
+def insert_ml100k_movies():
+    raw_movies_db_path = os.path.join(ds_ml100k_opt.get_working_dir(), 'raw', 'ml-latest-small', 'movies.csv')
     df_raw_movies = pd.read_csv(raw_movies_db_path)
     print(df_raw_movies.head())
+    insert_dataframe(df_raw_movies, "ml100k_movie")
 
-    with neo4jdb.get_session() as session:
-        for i, row in df_raw_movies.iterrows():
-            json_data = json.loads(row.to_json())
-            json_data['genres'] = json_data['genres'].split("|")
-            neo4jdb.insert_data(session, type='Movie', json_attrs=json.dumps(json_data))
+
+def insert_ml100k_users():
+    insert_list([i for i in range(1, 672)], "ml100k_user")
+
+
+def insert_ml100k_ratings():
+    raw_ratings_db_path = os.path.join(ds_ml100k_opt.get_working_dir(), 'raw', 'ml-latest-small', 'ratings.csv')
+    df_raw_ratings = pd.read_csv(raw_ratings_db_path)
+    insert_dataframe(df_raw_ratings, "ml100k_rating")
+
+
+def insert_ml100k_tags():
+    raw_movies_db_path = os.path.join(ds_ml100k_opt.get_working_dir(), 'raw', 'ml-latest-small', 'movies.csv')
+    df_raw_movies = pd.read_csv(raw_movies_db_path)
+    print(df_raw_movies.head())
+    insert_dataframe(df_raw_movies, "ml100k_movie")
+
+
+def insert_dataframe(df, type):
+    for i, series in df.iterrows():
+        node = Node(type)
+        for key, value in series.items():
+            node[key] = value
+        graph.create(node)
+        print("create node", node)
+
+
+def insert_list(list, type):
+    nodes = []
+    for i in list:
+        node = Node(type, id=i)
+        nodes.append(node)
+        graph.create(node)
+        print(f'created node {node} for type:{type}')
+
 
 if __name__ == '__main__':
     download_ml100k()
-    insert_ml100k()
+    # insert_ml100k_movies()
+    # insert_ml100k_users()
+    insert_ml100k_ratings()
+    # insert_ml100k_tags()
